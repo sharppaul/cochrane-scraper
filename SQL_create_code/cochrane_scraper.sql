81d548a3621f62cff17bf8dee106c0d9d0945b6a
sharppaul <weerheim.paul@gmail.com>
2017-08-21 13:48:09 +0200

    Create cochrane_scraper.sql

---
+++ SQL_create_code/cochrane_scraper.sql
@@ -0,0 +1,37 @@
+--Create user paul, with the password. 
+--(Password is not to be secure, but just to have something in place. 
+-- The security is closing SQL for external connections, 
+-- as this tool does not need external access or anything.)
+CREATE USER 'paul'@'localhost' IDENTIFIED BY '[paul3514]';
+
+--Create the *database* named paul, this is NOT the user, it's just a bad naming scheme I used... sorry.
+CREATE DATABASE paul;
+
+# Grant user Paul priviliges to the paul.* tables.
+GRANT ALL PRIVILEGES ON paul.* TO 'paul'@'localhost';
+
+--Start using the paul database.
+USE paul;
+
+--Create the tables
+CREATE TABLE `refs` (
+  `cochrane_id` char(15) NOT NULL,
+  `reference_pubmed_id` char(15) NOT NULL,
+  `topic` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `refs_author` (
+  `pubmed_id` char(15) NOT NULL,
+  `author_first` varchar(50) DEFAULT NULL,
+  `author_last` varchar(50) DEFAULT NULL,
+  `first_gender` varchar(10) DEFAULT NULL,
+  `last_gender` varchar(10) DEFAULT NULL,
+  `first_probability` int(11) DEFAULT NULL,
+  `last_probability` int(11) DEFAULT NULL,
+  `title` varchar(300) DEFAULT NULL,
+  `year` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`pubmed_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--Ready to go?
